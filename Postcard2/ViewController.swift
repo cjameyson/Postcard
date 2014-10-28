f98c5bee8fa3ac3e521fb9a3cc599a953a9a8bdd
Chris Jameyson <chrisjameyson@gmail.com>
2014-10-28 05:34:01 -0500

    first rev of proj

--- Postcard2/ViewController.swift
+++ Postcard2/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameTextbox: UITextField!
+    @IBOutlet weak var messageTextbox: UITextField!
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func touchDown(sender: UITextField) {
+        button.enabled = true
+        button.setTitle("Send Mail", forState: UIControlState.Normal)
+        println("on touch down")
+    }
+
+    
+    @IBAction func buttonClicked(sender: UIButton) {
+        var name:String  = nameTextbox.text
+        var message:String = messageTextbox.text
+        messageLabel.text = message
+        messageTextbox.text = ""
+        messageTextbox.resignFirstResponder()
+        messageLabel.hidden = false
+        messageLabel.textColor = UIColor.redColor()
+        
+        // update button text
+        button.setTitle("Mail Sent", forState: UIControlState.Normal)
+        button.enabled = false;
+        
+    }
 
 }
 
